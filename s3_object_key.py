a629abc19f1a28e6cb9ad8f443dcf8a2841bdab6
Ibraheem Azeem <48335413+fullstack-solutions@users.noreply.github.com>
2020-03-05 15:31:15 +0100

    Update s3_object_key.py

--- s3_object_key.py
+++ s3_object_key.py
@@ -1,7 +1,7 @@
 import access_s3_bucket
 
 def get_keys(bucket_name):
-  resp = aws_utils.s3_data(bucket_name)
+  resp = access_s3_bucket.s3_data(bucket_name)
   for s3_obj in resp['Contents']:
     key = s3_obj['Key']
     return key
