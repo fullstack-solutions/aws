a03fc1b3bdcdb8e2a8fcdbdc33c104f25adedc4b
Ibraheem Azeem <48335413+fullstack-solutions@users.noreply.github.com>
2020-03-06 13:34:15 +0100

    Create new_attribute_dynamodb.py
    To add new attribute in Dynamodb using non key attribute first we need to query to get the id, then add attribute with values using put_item using its unique key value.-

---
+++ new_attribute_dynamodb.py
@@ -0,0 +1,39 @@
+import boto3
+import json
+
+client = boto3.Session(region_name='eu-west-1').client('dynamodb')
+boto3.resource('dynamodb')
+
+your_id = 'project-id'
+
+updateStatus = client.query(
+    TableName='Testing',
+    IndexName='project_id-index',
+    KeyConditionExpression="  #G = :project_id",
+    ExpressionAttributeNames={"#G": "project_id"},
+    ExpressionAttributeValues={
+        ":project_id": {"S": your_id}
+    }
+)
+test = updateStatus['Items'][0]
+deserializer = boto3.dynamodb.types.TypeDeserializer()
+python_data = {k: deserializer.deserialize(
+    v) for k, v in test.items()}
+python_data = python_data['project_id']
+
+
+response = client.update_item(
+    TableName='Testing',
+    Key={
+        'project_id': {'S': python_data},
+    },
+    UpdateExpression="set test1 = :t1, test2=:t2",
+    ExpressionAttributeValues={
+        ':t1': {'S': 'abcd1'},
+        ':t2': {'S': "My text"}
+    },
+    ReturnValues="UPDATED_NEW"
+
+)
+
+print(json.dumps(response, indent=4))
