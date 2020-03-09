40e75a043cecc0b10314257354fda3e3c724e0a7
Ibraheem Azeem <48335413+fullstack-solutions@users.noreply.github.com>
2020-03-09 10:52:24 +0100

    Update upload_list_dynamodb.py

--- upload_list_dynamodb.py
+++ upload_list_dynamodb.py
@@ -2,20 +2,17 @@ import boto3
 
 
 #Add and Upload list of objects on dynaomodb by serializing it
-def add_list_attribute(list_objects)
-       
-
-        serializer = boto3.dynamodb.types.TypeSerializer()
-        serialized_objects = serializer.serialize(list_objects)
-
-        response = session.update_item(
-                TableName='table_name',
-                Key={
-                    'id': {'S': ID},
-                },
-                UpdateExpression='set  linked_objects = :list_data',
-                ExpressionAttributeValues={
-                    ':list_data': serialized_objects
-                },
-                ReturnValues="UPDATED_NEW"
-            )
+def add_list_attribute(list_objects):
+       serializer = boto3.dynamodb.types.TypeSerializer()
+       serialized_objects = serializer.serialize(list_objects)
+       response = session.update_item(
+       TableName='table_name',
+       Key={
+       'id': {'S': ID},
+       },
+       UpdateExpression='set  linked_objects = :list_data',
+       ExpressionAttributeValues={
+       ':list_data': serialized_objects
+       },
+       ReturnValues="UPDATED_NEW"
+       )
