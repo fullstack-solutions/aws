86d33f3cae6bd2ceefb39f3e85cad867939fd895
Ibraheem Azeem <48335413+fullstack-solutions@users.noreply.github.com>
2020-03-09 11:03:00 +0100

    Update upload_list_dynamodb.py

--- upload_list_dynamodb.py
+++ upload_list_dynamodb.py
@@ -10,6 +10,8 @@ def add_list_attribute(list_objects):
               Key={
                      'id': {'S': ID},
               },
+              ##to append the list with new data using another expression i.e.
+              ## 'set  linked_objects = list_append(linked_objects, :list_data)'
               UpdateExpression='set  linked_objects = :list_data',
               ExpressionAttributeValues={
                      ':list_data': serialized_objects
